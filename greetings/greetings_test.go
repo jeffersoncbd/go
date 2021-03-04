eafddd9f7575f624d56fde27308bf59fe205f717
Jefferson Carlos <jeff.carlosbd@gmail.com>
2021-03-04 10:35:46 -0300

    test: add tests

---
+++ greetings/greetings_test.go
@@ -0,0 +1,24 @@
+package greetings
+
+import (
+  "testing"
+  "regexp"
+)
+
+func TestHelloName(t *testing.T) {
+  name := "Jefferson"
+  want := regexp.MustCompile(`\b`+name+`\b`)
+
+  msg, err := Hello("Jefferson")
+  if !want.MatchString(msg) || err != nil {
+    t.Fatalf(`Hello("Jefferson") = %q, %v, want match for %#q nil`, msg, err, want)
+  }
+}
+
+func TestHelloEmpty(t *testing.T) {
+  msg, err := Hello("")
+  if msg != "" || err == nil {
+    t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+  }
+}
+
